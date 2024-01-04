f7fc60d529060d413d4821a2e5c9466627a2215d
ManiVannan Murugesan <mmvmaniit@gmail.com>
2024-01-04 15:57:37 +0530

    React2Svg

--- src/components/Helloworld.js
+++ src/components/Helloworld.js
@@ -1,25 +1,42 @@
-import satori from 'satori';
+import { useEffect, useState } from "react";
+import satori from "satori";
 
-const fontData = fetch(('../Font/Roboto-Black.ttf')).then((res) => res.arrayBuffer());
+export default function ReactToSvg() {
+  const [Svg, setSvg] = useState(null);
 
-console.log("font data",fontData)
-
-const Svg = await satori(
-  
-  <div style={{ fontFamily: 'sans-serif', color: 'black' }}>
-    hello, world
-  </div>,
-  {
-    width: 600,
-    height: 400,
-    fonts:[{
-      name: 'Roboto',
-      weight: 400,
-      style: 'normal',
-      data:fontData
+  useEffect(() => {
+    async function fetchData() {
+      const request = await fetch("../Font/Roboto-Black.ttf");
+      console.log("request");
+      const fontData = await request.arrayBuffer();
+      console.log("fontData", fontData);
+      const _svg = await satori(
+        <div style={{ fontFamily: "sans-serif", color: "black" }}>
+          hello, world
+        </div>,
+        {
+          width: 600,
+          height: 400,
+          fonts: [
+            {
+              name: "Roboto",
+              weight: 400,
+              style: "normal",
+              data: fontData,
+            },
+          ],
+        }
+      );
+      console.log("_svg", _svg);
+      setSvg(_svg);
     }
-    ]
-  },
-);
 
-export default Svg;
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Svg />
+    </>
+  );
+}
--- src/pages/TextingUI/index.js
+++ src/pages/TextingUI/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Cart from "../../components/UIComponents/Cart"
-import Dashbord from '../../components/UIComponents/Trendingnews'
-import Svg from "../../components/Helloworld"
-import Helloworld from '../../components/Helloworld'
+import React from "react";
+import Cart from "../../components/UIComponents/Cart";
+import Dashbord from "../../components/UIComponents/Trendingnews";
+import ReactToSvg from "../../components/Helloworld";
+import Helloworld from "../../components/Helloworld";
 
 export default function index() {
   return (
     <div>
-<Svg/>
+      <ReactToSvg />
     </div>
-  )
+  );
 }
